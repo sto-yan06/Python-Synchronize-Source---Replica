9f0372bc87cbf5bf6e01341b4180c269e0fe359c
S.g <stoian.george06@gmail.com>
2023-09-21 18:10:29 +0200

    Editted Main.py

--- main.py
+++ main.py
@@ -1,102 +1,185 @@
 import os
 import time
 import shutil
+import hashlib
+import datetime
+import argparse
+import threading
 
 
-directory = os.getcwd()
-source = 'source'
-replica = 'replica'
-sync_interval = 3600
+class Pathes_as_parameters():
 
-class ReplicaSourceLog():
+#With this class I made it so the path can be given by the users, and if Replica Directory and LogFile don't exist they are also automatically created
 
-# In this class, I made a static method to check the existance of each directory and file
+    def parse_args(self):
+        parser = argparse.ArgumentParser(description="Folder Synchronization Tool")
+        parser.add_argument("--src", required=True, help="Source Folder path")
+        parser.add_argument("--dst",  required=True, help="Replica Folder path")
+        parser.add_argument("--log", required=True, help="Log File Path")
+        parser.add_argument('--interval', type=int, default=0, help="Set the time interval for the code so it can run again (time in minutes please), 0 = One-Time Run")
+        return parser.parse_args()
 
     @staticmethod
-    def checkifexistSource():
-        if os.path.exists('source'):
-            print('Existing Directory...')
-        else:
-            os.makedirs('source')
-            print('Created New Source Directory')
+    def Source_given(args):
+        source_directory_path = args.src
+        dir = os.path.join(source_directory_path)
+        return source_directory_path
+
+
+    @staticmethod
+    def Replica_path(args): 
+        replica_directory_path = args.dst
+        dir = os.path.join(replica_directory_path, 'Replica')
+        
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+            print(f'No directory found. Created a Replica Directory in {dir}')
+        
+        return dir
+
     
     @staticmethod
-    def checkifexistReplica():
-        if os.path.exists('replica'):
-            print('Existing Directory...')
-        else:
-            os.makedirs('replica')
-            print('Created New Replica Directory')
+    def Log_file_path(args):
+        
+        log_file_path = args.log
+        log_file_location = os.path.join(log_file_path, 'LogFile.txt')
+
+        if not os.path.exists(log_file_location):
+            with open(log_file_location, 'w') as LogFile:
+                LogFile.write('Log File Created\n')
+
+        return log_file_location
+
+class ifModified():
+
+#In this class there is the static method to calculate the checksum size with MD5 Algorithm so it can be used in Folder Synchronization to see if there are any file changes
     
     @staticmethod
-    def checkifexistsLOGFILE():
-        logfile_path = os.path.join(directory, 'LogFile.txt')
+    def calculate_checksum(file_path):
+        md5_hash = hashlib.md5()
+        with open(file_path, 'rb') as file:
+            while chunk :=file.read(4096):
+                md5_hash.update(chunk)
+        return md5_hash.hexdigest()
+    
+    @staticmethod
+    def checktimestamp(file_path):
+        try:
+            timestamp = os.path.getmtime(file_path)
+            timestamp_date_time = datetime.datetime.fromtimestamp(timestamp)
+            return timestamp_date_time
+        except OSError:
+            print(f'Error: Could not get the time stamp for {file_path}')
 
-        if os.path.exists(logfile_path):
-            print('Existing Log File')
-        else:
-            with open(logfile_path, 'w') as LogFile:
-                print('Created a Log File')
+    
+class LoggingOperation():
 
-class FolderSynchronizationOperations():
+#In this class it's placed the logging operation method so in any cases it can be modified (it's Date Format, Lenght etc)
 
-    #In this class i made some methods for every synchronization step that I tought it would be necessary
+    def __init__(self, log_file_path):
+        self.log_file_path = log_file_path
 
-    def __init__(self):
-        self.source_path = os.path.join(directory, source)
-        self.replica_path = os.path.join(directory, replica)
-        self.log_path = os.path.join(directory, 'LogFile.txt')
-            
     def log_operation(self, message):
+
         timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
         log_message = f"[{timestamp}, {message}]"
-        with open(self.log_path, 'a') as log_file:
-            log_file.write(log_message + "\n")
+        with open(self.log_file_path, 'a') as LogFile:
+            LogFile.write(log_message + "\n")
         print(log_message)
+    
+class handleAllCases():
 
-# Here in the copy_item method it is used os and shutil libraries so using 'os' I could get the path for each file and directory and using 'shutil' I could manage the whole root directory
-# And made it also to log when a directory is overridden, just copied, or a file is copied
-# I also added an exception so I could identify the error easier  
+# This should be the main class where the comparison is happening, here are the 4 cases
+# Case 1: Create new files in the replica folder if there is any missing file from source
+# Case 2: Calculate checksum and get timestamp of each file in Source Folder and if there are no modifications nothing happens
+# Case 3: Calculate checksum and get timestamp of each file in Source folder and if there are any changes it updates the Replica Folder
+# Case 4: Checks to see if any file from Replica Folder is missing from Source, if it's missing then asks the user to delete it from Replica Folder as well or not
 
-    def copy_item(self, source_item, replica_item):
-        try:
-            if os.path.isdir(source_item):
-                if os.path.exists(replica_item):
-                    self.log_operation(f"Overriding Directory: {os.path.relpath(replica_item, self.replica_path)}")
-                    shutil.rmtree(replica_item)
-                shutil.copytree(source_item, replica_item)
-                self.log_operation(f"Directory Copied: {os.path.relpath(replica_item, self.replica_path)}")
-            else:
-                shutil.copy2(source_item, replica_item)
-                self.log_operation(f"File Copied: {os.path.relpath(replica_item, self.replica_path)}")
-        except Exception as e:
-            self.log_operation(f"Error copying {os.path.relpath(source_item, self.source_path)}: {str(e)}")
+    def __init__(self, args):
+        self.log_operation_instance = LoggingOperation(Pathes_as_parameters.Log_file_path(args))
+        self.log_operation = self.log_operation_instance.log_operation
 
-# folder_sync method uses the previous methods to check if there are existing directories and logfiles so the process could be done
-# after  checking it logs that a synchronization is happening and proceed to use the copy_item method for directories and files
+    def synchronize_folders(self, args):
 
-    def folder_sync(self):
+        self.log_operation_instance = LoggingOperation(Pathes_as_parameters.Log_file_path(args))
+        self.log_operation = self.log_operation_instance.log_operation
+
+        self.source_directory = Pathes_as_parameters.Source_given(args)
+        self.replica_directory = Pathes_as_parameters.Replica_path(args)
+        self.log_file_path = Pathes_as_parameters.Log_file_path(args)
+
+        for root, _, files in os.walk(self.source_directory):
+            for file_name in files:
+                source_file_path = os.path.join(root, file_name)
+                relative_path = os.path.relpath(source_file_path, self.source_directory)
+                replica_file_path = os.path.join(self.replica_directory, relative_path)
+
+                if not os.path.exists(replica_file_path):
+                    os.makedirs(os.path.dirname(replica_file_path), exist_ok=True)
+                    shutil.copy2(source_file_path, replica_file_path)
+                    self.log_operation(f"Created files into Replica Folder: {replica_file_path}")
+                else:
+                    source_checksum = ifModified.calculate_checksum(source_file_path)
+                    replica_checksum = ifModified.calculate_checksum(replica_file_path)
+                    source_timestamp = ifModified.checktimestamp(source_file_path)
+                    replica_timestamp = ifModified.checktimestamp(replica_file_path)
+
+                    if source_checksum != replica_checksum or source_timestamp != replica_timestamp:
+                        shutil.copy2(source_file_path, replica_file_path)
+                        self.log_operation(f"Updated {relative_path}")
+
+        for root, dirs, _ in os.walk(self.source_directory):
+            for dir_name in dirs:
+                source_dir_path = os.path.join(root, dir_name)
+                relative_path = os.path.relpath(source_dir_path, self.source_directory)
+                replica_dir_path = os.path.join(self.replica_directory, relative_path)
+
+                if not os.path.exists(replica_dir_path):
+                    os.makedirs(replica_dir_path)  # Create empty directories
+                    self.log_operation(f"Created empty directory in Replica Folder: {replica_dir_path}")
         
-        replica_log = ReplicaSourceLog()
-        replica_log.checkifexistReplica()
-        replica_log.checkifexistSource()
-        replica_log.checkifexistSource()
+        for root, dirs, files in os.walk(self.replica_directory):
+            for file_name in files:
+                replica_file_path = os.path.join(root, file_name)
+                relative_path = os.path.relpath(replica_file_path, self.replica_directory)
+                source_file_path = os.path.join(self.source_directory, relative_path)
 
-        self.log_operation("Synchronizing Folders...")
+                if not os.path.exists(source_file_path):
+                    user_input = input(f"Do you want to delete {relative_path} ? (y/n) ")
+                    if user_input == 'y':
+                        os.remove(replica_file_path)
+                        self.log_operation(f"Deleted {relative_path} from Replica Folder")
 
-        for root, dirs, files in os.walk(self.source_path):
-            for directory in dirs:
-                source_dir = os.path.join(root, directory)
-                replica_dir = os.path.join(self.replica_path, os.path.relpath(source_dir, self.source_path))
-                self.copy_item(source_dir, replica_dir)
+            for dir_name in dirs:
+                replica_dir_path = os.path.join(root, dir_name)
+                relative_path = os.path.relpath(replica_dir_path, self.replica_directory)
+                source_dir_path = os.path.join(self.source_directory, relative_path)
 
-            for file in files:
-                source_file = os.path.join(root, file)
-                replica_file = os.path.join(self.replica_path, os.path.relpath(source_file, self.source_path))
-                self.copy_item(source_file, replica_file)
+                if not os.path.exists(source_dir_path):
+                    user_input = input(f"Do you want to remove the directory {relative_path} ? (y/n)")
+                    if user_input == 'y':
+                        shutil.rmtree(replica_dir_path)
+                        self.log_operation(f"Delete directory {relative_path} from Replica Folder")
+
+        self.log_operation("Synchronization Complete")
+
+
+#main running method
+
+def main():
+    args = Pathes_as_parameters().parse_args()
+
+    if args.interval > 0:
+        try:
+            while True:
+                handler = handleAllCases(args)
+                handler.synchronize_folders(args)
+                time.sleep(args.interval * 60)
+        except KeyboardInterrupt:
+            print("Folder Synchronization Stopped")
+    else:
+        handler = handleAllCases(args)
+        handler.synchronize_folders(args)
 
 if __name__ == "__main__":
-    while True:
-        folder_items = FolderSynchronizationOperations()
-        folder_items.folder_sync()
-        time.sleep(sync_interval)     # It uses the input given as seconds to sleep between running the code again so it can synchronize periodically
+    main()
